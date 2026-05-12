bca96e4574976a105f3ec115884989c3b4be1a64
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-05-12 17:12:33 +0700

    data9

---
+++ inheritance.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// pembuatan class orang (Base Class)
+class orang {
+public:
+    // deklarasi member variabel dari class orang
+    string nama;
+
+    // pembuatan constructor dan destructor dari class orang
+    orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+    // pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
