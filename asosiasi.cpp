f8f3d7a29c506f135d909639f1eeb9956085af02
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-05-12 17:10:04 +0700

    data5

---
+++ asosiasi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// deklarasi class dokter agar bisa digunakan di dalam class pasien
+class dokter;
+
+// deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    // deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    // pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+
+    // deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
